5bf8836a324161859568634fdcaa9389b77dd596
NITHIN SAJI <nithinsaji33@gmail.com>
2020-11-20 23:13:03 +0530

    Deploying

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 // define path for express
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewPath = path.join(__dirname,'../templates/views')
@@ -79,6 +81,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('server is up at port 3000')
+app.listen(port,()=>{
+    console.log('server is up at port ' + port)
 })
